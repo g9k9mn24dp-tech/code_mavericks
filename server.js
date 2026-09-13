3c6bebb0253897c3643aa11093eb773abb4a48bc
g9k9mn24dp-tech <g9k9mn24dp@privaterelay.appleid.com>
2026-09-13 18:45:20 +0100

    Simplify Dungeon Realm to single purchase

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const stripe = process.env.STRIPE_SECRET_KEY ? new Stripe(process.env.STRIPE_SEC
 const prices = {
   "literally-illiterate": process.env.STRIPE_PRICE_LITERALLY,
 
-  "dungeon-adventurer-pack": process.env.STRIPE_PRICE_DUNGEON_ADVENTURER,
-  "dungeon-campaign-pack": process.env.STRIPE_PRICE_DUNGEON_CAMPAIGN,
-  "dungeon-founders-pack": process.env.STRIPE_PRICE_DUNGEON_FOUNDERS
+  "dungeon-realm-online": process.env.STRIPE_PRICE_DUNGEON_REALM
 };
 
 // Stripe webhook must use raw body before express.json().
